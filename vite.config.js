393c07f209642e64081b1361d6f0eda2d20e6392
Andreas Milch <mail@andreasmilch.cc>
2023-07-08 13:07:30 +0200

    setup to proxy api in vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import * as path from 'path';
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
     }
   }
 })
